ed7ac4d7348f23694262c0adf4299801eee6ca29
Dmitry Voronin <account@voronind.com>
2024-03-04 12:15:12 +0300

    Tmux : Use single file for syntax highlight.

--- module/common/tmux/Environment.nix
+++
@@ -1,6 +0,0 @@
-{ ... }: {
-	programs.tmux.extraConfig = ''
-		# Update environment from client.
-		set-option -g update-environment "SSH_CLIENT SSH_TTY DOCKER_CONFIG DOCKER_HOST DISPLAY XAUTHORITY"
-	'';
-}
