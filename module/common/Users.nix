7911bc4548e62d0a2b68462e0ca7470ce2faca17
Dmitry Voronin <account@voronind.com>
2024-03-04 12:32:38 +0300

    Users : Remove old user-dirs etc file.

--- module/common/Users.nix
+++ module/common/Users.nix
@@ -1,13 +1,3 @@
 { environment, ... }: {
 	users.mutableUsers = false;
-	environment.etc."user-dirs.dirs".text = ''
-		XDG_DESKTOP_DIR="$HOME/"
-		XDG_DOWNLOAD_DIR="$HOME/download"
-		XDG_TEMPLATES_DIR="$HOME/document/template"
-		XDG_PUBLICSHARE_DIR="$HOME/"
-		XDG_DOCUMENTS_DIR="$HOME/document"
-		XDG_MUSIC_DIR="$HOME/"
-		XDG_PICTURES_DIR="$HOME/media/picture"
-		XDG_VIDEOS_DIR="$HOME/media/video"
-	'';
 }
