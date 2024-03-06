d37abd9650768d6d5ed1133b8bd1b7dfba2991fc
Dmitry Voronin <account@voronind.com>
2024-03-06 04:01:54 +0300

    AutoUpdate : Switch instead of boot.

--- module/common/AutoUpdate.nix
+++ module/common/AutoUpdate.nix
@@ -1,10 +1,10 @@
-{ pkgs, inputs, lib, config, ... }: {
+{ ... }: {
 	system.autoUpgrade = {
 		enable      = true;
 		allowReboot = false;
 		dates       = "03:00";
 		flake       = "git+https://git.voronind.com/voronind/nixos.git";
-		operation   = "boot";
+		operation   = "switch";
 		persistent  = true;
 	};
 }
