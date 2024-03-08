1028cb2d88923d4a141e44fa9fc277b5b1ec07b2
Dmitry Voronin <account@voronind.com>
2024-03-08 20:28:40 +0300

    Nvim : Add <Leader>' to toggle tab width.

--- module/common/nvim/key/Common.lua
+++ module/common/nvim/key/Common.lua
@@ -4,6 +4,24 @@ function bye()
 	vim.cmd[[qa!]]
 end
 
+function toggle_tab_width()
+	if vim.bo.shiftwidth == 2 then
+		vim.bo.shiftwidth  = 4
+		vim.bo.tabstop     = 4
+		vim.bo.softtabstop = 4
+	elseif vim.bo.shiftwidth == 4 then
+		vim.bo.shiftwidth  = 8
+		vim.bo.tabstop     = 8
+		vim.bo.softtabstop = 8
+	elseif vim.bo.shiftwidth == 8 then
+		vim.bo.shiftwidth  = 2
+		vim.bo.tabstop     = 2
+		vim.bo.softtabstop = 2
+	end
+end
+
+rekey_normal("<Leader>'", "<cmd>lua toggle_tab_width()<cr>")
+
 -- Repeat previous command.
 rekey_normal("<Leader>.", "@:")
 rekey_visual("<Leader>.", "@:")
