9e9dbc85916b0326e53605de39c76d45f98d56b4
Dmitry Voronin <account@voronind.com>
2024-03-09 19:30:13 +0300

    Ssh : Add ssh_keygen function.

--- module/common/bash/module/Ssh.sh
+++ module/common/bash/module/Ssh.sh
@@ -1,7 +1,12 @@
 # Mount FS over ssh. Just extra config for sshfs.
-# Usage: sshmount <REMOTE> <LOCAL>
-function sshmount() {
+# Usage: ssh_mount <REMOTE> <LOCAL>
+function ssh_mount() {
 	sshfs -o uid=${UID},auto_unmount "${@}"
+}
+
+# Generate private an public keys on a local host and print a public key.
+function ssh_keygen() {
+	ssh-keygen -f ~/.ssh/key && cat ~/.ssh/key.pub
 }
 
 complete -o nospace -F _sshfs sshmount
