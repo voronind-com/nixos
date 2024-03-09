40b0fb5b0f704a42139b83cfdf72fbe063c432e9
Dmitry Voronin <account@voronind.com>
2024-03-09 19:42:03 +0300

    Nix : Temporarily disable subs.

--- module/common/Nix.nix
+++ module/common/Nix.nix
@@ -7,10 +7,11 @@
 		keep-derivations = true
 		keep-outputs = true
 		min-free = ${toString (50 * 1024 * 1024 * 1024)}
-		extra-substituters = ssh-ng://home
-		extra-trusted-substituters = ssh-ng://home
-		extra-trusted-public-keys = home:i9dE2VZI40/LCM/mmUGs8kwal1bQo3xbSjrzxhInmOw=
 	'';
+
+		# extra-substituters = ssh-ng://home
+		# extra-trusted-substituters = ssh-ng://home
+		# extra-trusted-public-keys = home:i9dE2VZI40/LCM/mmUGs8kwal1bQo3xbSjrzxhInmOw=
 	# max-free = ${toString (10 * 1024 * 1024 * 1024)}
 	# nix.gc = {
 	# 	automatic  = true;
