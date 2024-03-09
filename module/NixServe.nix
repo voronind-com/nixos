233d6e5aaab2979badc9285cf5970df04fc052de
Dmitry Voronin <account@voronind.com>
2024-03-09 17:28:41 +0300

    NixServe : Hide key directory.

--- module/NixServe.nix
+++ module/NixServe.nix
@@ -4,6 +4,6 @@
 		bindAddress   = "0.0.0.0";
 		package       = pkgs.nix-serve-ng;
 		port          = 5000;
-		secretKeyFile = "/root/nixcache/secret-key-file"; # Generate with: nix-store --generate-binary-cache-key nixcache.voronind.com secret-key-file public-key-file
+		secretKeyFile = "/root/.nixcache/secret-key-file"; # Generate with: nix-store --generate-binary-cache-key nixcache.voronind.com secret-key-file public-key-file
 	};
 }
