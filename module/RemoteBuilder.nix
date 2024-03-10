5795283bf6f085988be9c1c100f1de0104378573
Dmitry Voronin <account@voronind.com>
2024-03-10 06:10:43 +0300

    RemoteBuilder : Add extra nix config to cache builds.

--- module/RemoteBuilder.nix
+++ module/RemoteBuilder.nix
@@ -2,5 +2,6 @@
 	# To apply: nix sign-paths --all -k /path/to/secret-key-file
 	nix.extraOptions = ''
 		extra-secret-key-files = /root/.nixcache/secret-key-file
+		builders-use-substitutes = true
 	'';
 }
