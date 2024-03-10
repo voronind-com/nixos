420663df787908186752971bf738ad0dbbefaeeb
Dmitry Voronin <account@voronind.com>
2024-03-10 07:33:46 +0300

    RemoteBuilder : Remove extra- from secret key files.

--- module/RemoteBuilder.nix
+++ module/RemoteBuilder.nix
@@ -1,6 +1,6 @@
 { ... }: {
 	# To apply: nix store sign --all -k /path/to/secret-key-file
 	nix.extraOptions = ''
-		extra-secret-key-files = /root/.nixcache/secret-key-file
+		secret-key-files = /root/.nixcache/secret-key-file
 	'';
 }
