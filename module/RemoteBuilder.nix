2aec98e5a3cfb302abd609861c0dafe8c1c0c62d
Dmitry Voronin <account@voronind.com>
2024-03-10 06:46:02 +0300

    RemoteBuilder : Remove unneded config.

--- module/RemoteBuilder.nix
+++ module/RemoteBuilder.nix
@@ -1,7 +1,6 @@
 { ... }: {
-	# To apply: nix sign-paths --all -k /path/to/secret-key-file
+	# To apply: nix store sign --all -k /path/to/secret-key-file
 	nix.extraOptions = ''
 		extra-secret-key-files = /root/.nixcache/secret-key-file
-		builders-use-substitutes = true
 	'';
 }
