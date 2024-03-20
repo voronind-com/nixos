4107c197699bc92e0c2127dc2771a37d47ed9a7c
Dmitry Voronin <account@voronind.com>
2024-03-20 22:33:37 +0300

    AmdGpu : Enable ROCm support.

--- module/AmdGpu.nix
+++ module/AmdGpu.nix
@@ -1,9 +1,19 @@
-{ pkgs, ... }: {
+{ nixpkgs, pkgs, ... }: {
 	boot.initrd.kernelModules     = [ "amdgpu" ];
 	services.xserver.videoDrivers = [ "amdgpu" ];
 	hardware.opengl.driSupport      = true;
 	hardware.opengl.driSupport32Bit = true;
 	environment.variables.AMD_VULKAN_ICD = "RADV";
+
+	# Compute.
+	nixpkgs.config.rocmSupport = true;
+	systemd.tmpfiles.rules = [
+		"L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+	];
+	hardware.opengl.extraPackages = with pkgs; [
+		rocmPackages.clr.icd
+	];
+
 	# AMDVLK was broken for me (huge stuttering). So keep it disabled, at least for now.
 	# hardware.opengl.extraPackages = with pkgs; [
 	# 	amdvlk
