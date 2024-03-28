cd9615c5719685cb566e0702a083f23189f47995
Dmitry Voronin <account@voronind.com>
2024-03-28 11:02:30 +0300

    Sway : Add gtk portal.

--- module/Sway.nix
+++ module/Sway.nix
@@ -43,16 +43,25 @@
 	# Portals.
 	xdg.portal = {
 		enable = true;
+		extraPortals = with pkgs; [
+			xdg-desktop-portal-gnome
+			xdg-desktop-portal-gtk
+			xdg-desktop-portal-kde
+		];
 		config = {
 			common = {
 				default = [
 					"gtk"
+					"wlr"
+					"gnome"
+					"kde"
 				];
 			};
 		};
 		wlr = {
 			enable = true;
 		};
+		xdgOpenUsePortal = true;
 	};
 
 	# Brightness control.
