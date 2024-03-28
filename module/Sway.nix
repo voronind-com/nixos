3397a1a249f90413f606c8da04a98d9ca6e8a336
Dmitry Voronin <account@voronind.com>
2024-03-28 11:50:47 +0300

    Sway : Enable bluetooth hw.

--- module/Sway.nix
+++ module/Sway.nix
@@ -69,7 +69,9 @@
 	programs.light.enable = true;
 
 	# Bluetooth.
-	services.blueman.enable = true;
+	hardware.bluetooth.enable      = true;
+	hardware.bluetooth.powerOnBoot = true;
+	services.blueman.enable        = true;
 
 	# Configs.
 	environment.variables = {
