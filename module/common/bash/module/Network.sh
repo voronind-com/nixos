aabe449f5979f5093aaeb0d253c2e84ea1613854
Dmitry Voronin <account@voronind.com>
2024-03-30 09:52:18 +0300

    Network : Add ns, nu and nd.

--- module/common/bash/module/Network.sh
+++ module/common/bash/module/Network.sh
@@ -7,3 +7,26 @@ function bluetooth() {
 function network() {
 	nm-connection-editor
 }
+
+# Show active connections.
+function ns() {
+	nmcli connection show
+}
+
+# Start the connection.
+# Usage: nu <CONNECTION>
+function nu() {
+	nmcli connection up "${@}"
+}
+
+# Stop the connection.
+# Usage: nd <CONNECTION>
+function nd() {
+	nmcli connection down "${@}"
+}
+
+function _complete_connections() {
+	_autocomplete $(nmcli connection show | sed "1d" | cut -d\  -f1)
+}
+
+complete -F _complete_connections nd nu
