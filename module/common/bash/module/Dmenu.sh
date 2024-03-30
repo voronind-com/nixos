8519c17087ee1ad837e678690a9ce45a4bed34c4
Dmitry Voronin <account@voronind.com>
2024-03-30 05:30:36 +0300

    Sway : Add launcher prompt.

--- module/common/bash/module/Dmenu.sh
+++ module/common/bash/module/Dmenu.sh
@@ -1,6 +1,11 @@
 # Wrapped dmenu_path to include my functions.
 function _dmenu_path_wrapped() {
-	echo
+	c=0
+	while [ ${c} -lt 1000 ]; do
+		echo
+		((c++))
+	done
+
 	dmenu_path
 	find_function | grep -v ^_
 }
