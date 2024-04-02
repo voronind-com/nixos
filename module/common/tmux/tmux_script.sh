f4494a11f417f7d4a67541c868059f2c0c88fbf6
Dmitry Voronin <account@voronind.com>
2024-04-02 07:42:51 +0300

    Tmux : Add volume to status-bar.

--- module/common/tmux/tmux_script.sh
+++ module/common/tmux/tmux_script.sh
@@ -15,7 +15,6 @@ function _tmux_battery() {
 	[[ "${status}" = "Charging" ]] && is_charging=true
 
 	local icon
-
 	if ((level>=60))
 	then
 		icon="󱊣"
@@ -29,15 +28,38 @@ function _tmux_battery() {
 		icon="󰂎"
 	fi
 
-	echo -n " ${level} ${icon}"
+	echo -n "${icon}"
 	${is_charging} && echo -n "󱐋"
-	echo -n " "
+	[[ "${level}" -lt 40 ]] && echo -n " ${level}"
+}
+
+function _tmux_volume() {
+	local IFS=$'\n'
+	local level=$(wpctl get-volume @DEFAULT_SINK@ | cut -d\  -f2 | sed -e "s/^0//" -e "s/\.//")
+	[[ "${level}" = "00" ]] && level="0"
+
+	local icon
+	if ((level>=80))
+	then
+		icon="󰕾"
+	elif ((level>=40))
+	then
+		icon="󰖀"
+	elif ((level>=10))
+	then
+		icon=""
+	else
+		icon="󰝟"
+	fi
+
+	wpctl get-volume @DEFAULT_SINK@ | grep '[MUTED]' && icon="󰸈"
+
+	echo -n "${icon}"
+	[[ "${level}" -gt 100 ]] && echo -n " ${level}%"
 }
 
 case ${action} in
-	"battery")
-		_tmux_battery
-		;;
-	"*")
-		;;
+	"battery") _tmux_battery ;;
+	"volume")  _tmux_volume  ;;
+	"*") ;;
 esac
