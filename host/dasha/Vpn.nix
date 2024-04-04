b05c512d29b1ae0fb0553801e0f420850552f0d7
Dmitry Voronin <account@voronind.com>
2024-04-05 01:09:34 +0300

    Dasha : Fix vpn.

--- host/dasha/Vpn.nix
+++ host/dasha/Vpn.nix
@@ -6,4 +6,16 @@
 		# strongswan
 		# strongswanNM
 	];
+	networking.networkmanager.enableStrongSwan = true;
+	services.xl2tpd.enable = true;
+	services.strongswan = {
+		enable = true;
+		secrets = [
+			"ipsec.d/ipsec.nm-l2tp.secrets"
+		];
+	};
+	# NOTE: Try this if VPN ever breaks.
+	# systemd.tmpfiles.rules = [
+	# 	"L /etc/ipsec.secrets - - - - /etc/ipsec.d/ipsec.nm-l2tp.secrets"
+	# ];
 }
