88b0c7d3139b2c9f5c3780c6a0562ae02160760e
Dmitry Voronin <account@voronind.com>
2024-04-08 19:40:53 +0300

    Wallpaper : Desert.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://i.imgur.com/ZcXlrK9.png";
-	sha256 = "sha256-uh0GnrzjushvPmx75RIOzNC++5H1CNR5ZDm2lUWijYc=";
+	url    = "https://images.unsplash.com/photo-1542401886-65d6c61db217";
+	sha256 = "sha256-iSDB8YqemQUwJ8Fe8jh5nyWXtXkB02z+zOl+YmIWSkE=";
 in {
 	path = pkgs.fetchurl {
 		url    = url;
