d7950902492d4f9589936317abca009cd2d8b3ae
Dmitry Voronin <hi@voronind.com>
2024-04-14 01:21:45 +0300

    Wallpaper : Moon boy.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://images.unsplash.com/photo-1542401886-65d6c61db217";
-	sha256 = "sha256-iSDB8YqemQUwJ8Fe8jh5nyWXtXkB02z+zOl+YmIWSkE=";
+	url    = "https://4kwallpapers.com/images/wallpapers/boy-kid-alone-silhouette-moon-night-clouds-illustration-5846x4134-1017.jpg";
+	sha256 = "sha256-MflCIvvK10vNoV6YN+I5cPKcKZLeIGYJIXePWjWSn8g=";
 in {
 	path = pkgs.fetchurl {
 		url    = url;
