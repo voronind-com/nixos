7e8d23496ba9bb93c7a5d2737795ca8d1e74c9cb
Dmitry Voronin <account@voronind.com>
2024-04-15 17:58:37 +0300

    Disk : Add unlock function.

--- module/common/bash/module/Disk.nix
+++ module/common/bash/module/Disk.nix
@@ -11,5 +11,20 @@
 		function tdu() {
 			du -sh --si "''${@}"
 		}
+
+		# Unlock encrypted disk file.
+		# Usage: unlock <FILE>
+		function unlock() {
+			_filter() {
+				sed -e "s/.*\ a[st]\ //" -e "s/\.$//"
+			}
+			local file="''${1}"
+			local name=$(parse_alnum ''${file} | _filter)
+			local loop=$(udisksctl loop-setup -f "''${file}" | _filter)
+			local unlock=$(udisksctl unlock -b "''${loop}" | _filter)
+			local mount=$(udisksctl mount -b "''${unlock}" | _filter)
+
+			cd "''${mount}"
+		}
 	'';
 }
