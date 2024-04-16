191e3ffbfbc86b20eb655362a400ab7efd6e7b3c
Dmitry Voronin <hi@voronind.com>
2024-04-16 15:15:25 +0300

    Wallpaper : Pixel spring city backyard Vaneela.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://4kwallpapers.com/images/wallpapers/boy-kid-alone-silhouette-moon-night-clouds-illustration-5846x4134-1017.jpg";
-	sha256 = "sha256-MflCIvvK10vNoV6YN+I5cPKcKZLeIGYJIXePWjWSn8g=";
+	url    = "https://r4.wallpaperflare.com/wallpaper/1024/884/847/waneella-pixel-art-city-plants-hd-wallpaper-cffe8c9aa7605f8ab4ba9c6980e5fbe4.jpg";
+	sha256 = "sha256-a3cvyt7AuBZ2zUMMzgKAkLvjXyHZ4AkT8N6FrGMuxYk=";
 in {
 	path = pkgs.fetchurl {
 		url    = url;
