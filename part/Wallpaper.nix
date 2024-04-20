a092ecb5b4df493842817ce47377e1593c332c61
Dmitry Voronin <hi@voronind.com>
2024-04-21 00:56:56 +0300

    Wallpaper : Dasha aurora.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://r4.wallpaperflare.com/wallpaper/1024/884/847/waneella-pixel-art-city-plants-hd-wallpaper-cffe8c9aa7605f8ab4ba9c6980e5fbe4.jpg";
-	sha256 = "sha256-a3cvyt7AuBZ2zUMMzgKAkLvjXyHZ4AkT8N6FrGMuxYk=";
+	url    = "https://i.imgur.com/EjuHAgm.png";
+	sha256 = "sha256-glQj8IfDvmZ+rI0cnHhvopVxWOxHZLiahDSt9zVUN/E=";
 in {
 	path = pkgs.fetchurl {
 		url    = url;
