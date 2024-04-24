8f4f229314b645ef2f729088300e802a91abfc63
Dmitry Voronin <hi@voronind.com>
2024-04-25 00:58:09 +0300

    Keyd : Move macros to DRG app.

--- user/common/keyd/default.nix
+++ user/common/keyd/default.nix
@@ -15,5 +15,22 @@
 		alt.s = down
 		alt.d = right
 		alt.space = f6
+
+		[steam_app_548430]
+		control.w = macro(enter 10ms r enter)
+		control.a = macro(enter 10ms N o enter)
+		control.s = macro(enter 10ms W a i t enter)
+		control.d = macro(enter 10ms Y e s enter)
+		control.q = macro(enter 10ms S o r r y ! enter)
+		control.e = macro(enter 10ms T h a n k s ! enter)
+		control.g = macro(enter 10ms g g enter)
+		control.z = macro(enter 10ms < space L e f t enter)
+		control.x = macro(enter 10ms > space H e r e space < enter)
+		control.c = macro(enter 10ms > space R i g h t enter)
+		control.r = macro(enter 10ms N e e d space m o r e space a m m o enter)
+		control.t = macro(enter 10ms M a y space I space t a k e space e x t r a ? enter)
+		control.f = macro(enter 10ms A F K enter)
+		control.v = macro(enter 10ms I space n e e d space h e l p ! enter)
+		control.b = macro(enter 10ms I ' m space b a c k enter)
 	'';
 }
