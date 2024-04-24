a32f5e42183d22052e6215c4752227f2cbcd81dd
Dmitry Voronin <hi@voronind.com>
2024-04-24 06:16:18 +0300

    Keyd : Add keys for Firefox.

--- user/common/keyd/default.nix
+++ user/common/keyd/default.nix
@@ -1,7 +1,18 @@
 { util, ... }: {
 	config = util.trimTabs ''
 		[firefox]
-		alt.q = macro(C-S-tab)
-		alt.e = macro(C-tab)
+		alt.q = C-S-tab
+		alt.e = C-tab
+		alt.x = C-w
+		alt.t = C-t
+		alt.r = C-S-t
+		alt.f = C-f
+		alt.n = f3
+		alt.p = S-f3
+		alt.w = up
+		alt.a = left
+		alt.s = down
+		alt.d = right
+		alt.space = f6
 	'';
 }
