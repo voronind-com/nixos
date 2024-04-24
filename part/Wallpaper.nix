e3ebfe9bcddee8426ce5d68fdf71bfbf5d7cfb0e
Dmitry Voronin <hi@voronind.com>
2024-04-24 08:54:58 +0300

    Wallpaper : Cyberpunk ship.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://i.imgur.com/EjuHAgm.png";
-	sha256 = "sha256-glQj8IfDvmZ+rI0cnHhvopVxWOxHZLiahDSt9zVUN/E=";
+	url    = "https://r4.wallpaperflare.com/wallpaper/519/595/676/cyberpunk-video-games-pixel-art-the-last-night-wallpaper-9261d2d9e319dfea44788a19b8657ecf.jpg";
+	sha256 = "sha256-ZKdV2eY+rDJZzG0JN8yMvnP2FiqGzvA8HKRqtA9Xnjs=";
 in {
 	path = pkgs.fetchurl {
 		url    = url;
