714d73eb18a746207ded5828a2f53da9db257ecc
Dmitry Voronin <hi@voronind.com>
2024-04-25 04:45:57 +0300

    Sway : Lock screen before sleep.

--- module/sway/module/Session.nix
+++ module/sway/module/Session.nix
@@ -1,6 +1,6 @@
 { key, style, ... }: {
 	text = ''
-		bindsym $mod+${key.action.wait} exec '_twice 1 swaylock -F -c 000000 -k --font "${style.font.serif.name}" --font-size ${toString(style.font.size.desktop)}'
-		bindsym $mod+Shift+${key.action.exit} exec '_twice 1 systemctl poweroff -i'
+		bindsym $mod+${key.action.wait} exec '_twice 1 swaylock -f -F -c 000000 -k --font "${style.font.serif.name}" --font-size ${toString(style.font.size.desktop)}'
+		bindsym $mod+Shift+${key.action.exit} exec _twice 1 bash -c 'swaylock -f -F -c 000000 -k --font "${style.font.serif.name}" --font-size ${toString(style.font.size.desktop)}; systemctl suspend -i'
 	'';
 }
