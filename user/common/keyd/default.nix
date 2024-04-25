db020833c897aff3df92b8416de20ba62609d804
Dmitry Voronin <hi@voronind.com>
2024-04-25 03:13:27 +0300

    Keyd : DRG add good job macro.

--- user/common/keyd/default.nix
+++ user/common/keyd/default.nix
@@ -33,5 +33,6 @@
 		alt.f = macro(enter 10ms I space n e e d space h e l p ! enter)
 		alt.b = macro(enter 10ms I ' m space b a c k enter)
 		alt.l = macro(enter 10ms L e a f space l o v e r enter)
+		alt.j = macro(enter 10ms G o o d space j o b enter)
 	'';
 }
