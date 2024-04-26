420b46e6f01e18e6271243daaa095239ff83a271
Dmitry Voronin <hi@voronind.com>
2024-04-27 00:45:07 +0300

    Sway : Refactor session.

--- module/sway/module/Session.nix
+++ module/sway/module/Session.nix
@@ -1,6 +1,8 @@
-{ key, style, ... }: {
+{ key, style, ... }: let
+	lock = "swaylock -f -F -c 000000 -k --font \"${style.font.serif.name}\" --font-size ${toString(style.font.size.desktop)}";
+in {
 	text = ''
-		bindsym $mod+${key.action.wait} exec '_twice 1 swaylock -f -F -c 000000 -k --font "${style.font.serif.name}" --font-size ${toString(style.font.size.desktop)}'
-		bindsym $mod+Shift+${key.action.exit} exec _twice 1 bash -c 'swaylock -f -F -c 000000 -k --font "${style.font.serif.name}" --font-size ${toString(style.font.size.desktop)}; systemctl suspend -i'
+		bindsym $mod+${key.action.wait} exec '_twice 1 ${lock}'
+		bindsym $mod+Shift+${key.action.exit} exec _twice 1 bash -c '${lock}; systemctl suspend -i'
 	'';
 }
