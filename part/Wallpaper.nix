44c9779431580ada9d1dea741b66cf0818de70a2
Dmitry Voronin <hi@voronind.com>
2024-04-27 00:48:53 +0300

    Wallpaper : Airport.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://r4.wallpaperflare.com/wallpaper/611/415/768/forest-pixel-art-wallpaper-2960a87db18add1bb637d87f20a1e6ed.jpg";
-	sha256 = "sha256-sfi4hbpfi27SoOjDuEW5FXpuat51poQXawVFrni0z7w=";
+	url    = "https://r4.wallpaperflare.com/wallpaper/393/521/87/artistic-pixel-art-aircraft-airport-hd-wallpaper-c8661d782010dca8102cf14eb832349a.jpg";
+	sha256 = "sha256-yyiLWLJJq1N0sTO3lDP1ze0XS8p9EW+c7CSQdP1m/vQ=";
 in {
 	path = pkgs.fetchurl {
 		url    = url;
