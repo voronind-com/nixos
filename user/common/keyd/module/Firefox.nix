cfd7767e697f0535e4793994bb661519a94583e7
Dmitry Voronin <account@voronind.com>
2024-04-30 19:05:35 +0300

    Keyd : Add Firefox Dark Reader & Bitwarden toggles.

--- user/common/keyd/module/Firefox.nix
+++ user/common/keyd/module/Firefox.nix
@@ -1,19 +1,21 @@
 { ... }: {
 	text = ''
 		[firefox]
-		alt.q = C-S-tab
-		alt.e = C-tab
-		alt.x = C-w
-		alt.t = C-t
-		alt.r = C-f5
-		alt.u = C-S-t
-		alt.f = C-f
-		alt.n = f3
-		alt.p = S-f3
-		alt.w = up
 		alt.a = left
-		alt.s = down
 		alt.d = right
+		alt.e = C-tab
+		alt.f = C-f
+		alt.l = A-S-a
+		alt.n = f3
+		alt.p = C-S-l
+		alt.p = S-f3
+		alt.q = C-S-tab
+		alt.r = C-f5
+		alt.s = down
 		alt.space = f6
+		alt.t = C-t
+		alt.u = C-S-t
+		alt.w = up
+		alt.x = C-w
 	'';
 }
