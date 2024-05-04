1ad92a06c66ef4c46f23b4b495a0f55249b0f5bc
Dmitry Voronin <account@voronind.com>
2024-05-04 22:22:41 +0300

    Waybar : Fix workspaces background.

--- module/desktop/waybar/style/Workspace.nix
+++ module/desktop/waybar/style/Workspace.nix
@@ -2,10 +2,7 @@
 	text = ''
 		#workspaces button {
 			padding: 0 4px;
-			color: #${style.color.fg.light};
 			border-top: 2px solid transparent;
-			background-color: transparent;
-			background-color: rgba(${style.color.bg-r},${style.color.bg-g},${style.color.bg-b},${toString(style.opacity.desktop)});
 		}
 
 		#workspaces button.focused {
