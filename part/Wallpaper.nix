20d25fc08ab34a3c163c50543e40e6db79e7b4c8
Dmitry Voronin <hi@voronind.com>
2024-05-07 23:14:44 +0300

    Wallpaper : Pink lake.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://r4.wallpaperflare.com/wallpaper/154/699/724/super-time-force-pixels-pixel-art-wallpaper-0f85e20caec63c8b759cb842a10114b4.jpg";
-	sha256 = "sha256-w7n93e+Tz9t32raDWFtyPOuKfvy8d6X8Tq8bBZrQvqc=";
+	url    = "https://r4.wallpaperflare.com/wallpaper/500/198/463/pixel-art-digital-art-pixelated-pixels-wallpaper-b980f83df17add4b2677d8cf6091268d.jpg";
+	sha256 = "sha256-OeHKf2fk4mqUvJTylYnR1uukzJJrKVpyhRjPY4om/K4=";
 in {
 	path = pkgs.fetchurl {
 		url    = url;
