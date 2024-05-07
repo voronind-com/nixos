804fc6f604c28e3267cb076ae1a100518c5e2c2d
Dmitry Voronin <hi@voronind.com>
2024-05-08 02:08:02 +0300

    Idea : Add shortcuts.

--- user/common/keyd/module/Idea.nix
+++ user/common/keyd/module/Idea.nix
@@ -1,6 +1,18 @@
 { ... }: {
 	text = ''
 		[jetbrains-idea-ce]
+		alt.C = C-o
+		alt.D = C-A-5
+		alt.c = C-S-A-t
+		alt.d = S-f9
+		alt.e = A-right
+		alt.f = C-S-f
+		alt.g = macro(gd)
+		alt.q = A-left
+		alt.r = S-f10
 		alt.s = C-A-s
+		alt.v = C-q
+		alt.x = C-f4
+		alt.z = C-f2
 	'';
 }
