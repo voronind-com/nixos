8857f1169df4b7e12fd84b10c60ae3fcd7bf0551
Dmitry Voronin <hi@voronind.com>
2024-05-15 02:18:59 +0300

    Wallpaper : Fix broken readme ref.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: let
+	url    = "https://pixeldrain.com/api/file/ppeeEr4d";
+	sha256 = "sha256-CeKigHd9R6Q4axalcWjHySuOj1RuY68niJ4qoQai4rM=";
+
 	# Sometimes stylix does not generate enough contrast for text.
 	# This setting forces white text to ensure contrast on dark backgrounds.
 	forceWhiteText = true;
-
-	url    = "https://pixeldrain.com/api/file/ppeeEr4d";
-	sha256 = "sha256-CeKigHd9R6Q4axalcWjHySuOj1RuY68niJ4qoQai4rM=";
 in {
 	inherit forceWhiteText;
 
