e79bc71713c1d7e4578197deb3fc037c4143b125
Dmitry Voronin <hi@voronind.com>
2024-05-20 17:07:48 +0300

    Tmux : Use selection color for selection.

--- module/common/tmux/module/Copy.nix
+++ module/common/tmux/module/Copy.nix
@@ -1,9 +1,8 @@
 { key, style, ... }: let
 	mod = key.tmux.mod;
 
-	accent = style.color.accent;
-	bg     = style.color.bg.dark;
-	fg     = style.color.fg.light;
+	fg        = style.color.bg.dark;
+	selection = style.color.selection;
 in {
 	text = ''
 		setw -g mode-keys vi
@@ -12,6 +11,6 @@ in {
 		bind -T copy-mode-vi ${key.tmux.copy.select} send      -X begin-selection
 		bind -T copy-mode-vi ${key.tmux.copy.copy}   send-keys -X copy-pipe-and-cancel "pbcopy"
 
-		set -g mode-style "fg=#${bg} bg=#${accent} bold"
+		set -g mode-style "fg=#${fg} bg=#${selection} bold"
 	'';
 }
