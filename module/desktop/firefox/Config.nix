d12447c54f42d148882b9e0f3755e4cc8c00e8f7
Dmitry Voronin <hi@voronind.com>
2024-05-20 14:31:14 +0300

    Firefox : Add User agent switcher ext.

--- module/desktop/firefox/Config.nix
+++ module/desktop/firefox/Config.nix
@@ -35,5 +35,8 @@
 
 		// Formats.
 		lockPref("image.jxl.enabled", true);
+
+		// User agent.
+		// pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36");
 	'';
 }
