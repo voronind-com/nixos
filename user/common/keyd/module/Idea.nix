f8a198ff0d2d189533f3b9b7d5423200ae38c0fb
Dmitry Voronin <account@voronind.com>
2024-05-29 02:20:56 +0300

    Keyd : Add Idea panel close.

--- user/common/keyd/module/Idea.nix
+++ user/common/keyd/module/Idea.nix
@@ -2,7 +2,7 @@
 	text = ''
 		[jetbrains-idea-ce]
 		alt.a = C-A-5
-		alt.c = C-S-A-t
+		alt.c = S-escape
 		alt.d = S-f9
 		alt.e = A-right
 		alt.f = C-S-f
@@ -12,6 +12,7 @@
 		alt.r = S-f10
 		alt.s = C-A-s
 		alt.v = C-q
+		alt.w = C-S-A-t
 		alt.x = C-f4
 		alt.z = C-f2
 		alt./ = macro(C-/ up)
