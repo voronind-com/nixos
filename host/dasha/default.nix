dcf2745926a46e033f9b652742523bf46aefb9c3
Dmitry Voronin <hi@voronind.com>
2024-06-04 16:36:13 +0300

    Dasha : Enable keyd.

--- host/dasha/default.nix
+++ host/dasha/default.nix
@@ -4,6 +4,6 @@
 	];
 
 	# Disable keyd.
-	services.keyd.enable = lib.mkForce false;
-	systemd.services.keyd-application-mapper.enable = lib.mkForce false;
+	# services.keyd.enable = lib.mkForce false;
+	# systemd.services.keyd-application-mapper.enable = lib.mkForce false;
 }
