4df66b7f3cde54e38bacda088a76377edeadb3a5
Dmitry Voronin <hi@voronind.com>
2024-06-09 16:09:27 +0300

    Wallpaper : Huge wave.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://i.imgur.com/PYKK4Tg.jpeg";
-	sha256 = "sha256-lM/EHVPj0Yoxd86002YnFaWbWd48wBIJOFTv+eKJH1g=";
+	url    = "https://i.imgur.com/EpFhNuM.jpeg";
+	sha256 = "sha256-j6c5+f5/KgmGzNfZnMShAXrhbAOhsiNlr0DJh+puyQU=";
 
 	# Sometimes stylix does not generate enough contrast for text.
 	# This setting forces white text to ensure contrast on dark backgrounds.
