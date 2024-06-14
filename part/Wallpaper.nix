768564111aafeedefab0eea4460d710e2a3b9817
Dmitry Voronin <hi@voronind.com>
2024-06-15 01:00:28 +0300

    Wallpaper : Mont Blank.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://i.imgur.com/EpFhNuM.jpeg";
-	sha256 = "sha256-j6c5+f5/KgmGzNfZnMShAXrhbAOhsiNlr0DJh+puyQU=";
+	url    = "https://i.imgur.com/uxNas3C.jpeg";
+	sha256 = "sha256-OxixfSixKYDhNx2vxGV5fLeGrHHV1uXS4MJHLnCIFZs=";
 
 	# Sometimes stylix does not generate enough contrast for text.
 	# This setting forces white text to ensure contrast on dark backgrounds.
