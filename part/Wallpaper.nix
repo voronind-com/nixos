06991d8f5f0be59f73cb13f08a70987065f8a2e2
Dmitry Voronin <hi@voronind.com>
2024-06-23 01:29:43 +0300

    Wallpaper : Austroquarium.

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: let
-	url    = "https://i.imgur.com/CrX0Grv.png";
-	sha256 = "sha256-b9kHrg+EtLCQvIgpvv4hFp79jswAurntty3YnFKr5j0=";
+	url    = "https://i.imgur.com/1RVKYxq.jpeg";
+	sha256 = "sha256-9L2nlp2RpdIAKsoA9j1wgTF6WTkBYZCU0z7Rnn8pCvY=";
 
 	# Sometimes stylix does not generate enough contrast for text.
 	# This setting forces white text to ensure contrast on dark backgrounds.
-	forceContrastText = true;
+	forceContrastText = false;
 in {
 	inherit forceContrastText;
 
