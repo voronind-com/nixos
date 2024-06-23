5931624b203da43d2bd9313b62a925364ea3d44b
Dmitry Voronin <hi@voronind.com>
2024-06-23 03:23:57 +0300

    Wallpaper : Frosty Crabs.voronind

--- part/Wallpaper.nix
+++ part/Wallpaper.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: let
-	url    = "https://i.imgur.com/1RVKYxq.jpeg";
-	sha256 = "sha256-9L2nlp2RpdIAKsoA9j1wgTF6WTkBYZCU0z7Rnn8pCvY=";
+	url    = "https://i.imgur.com/rmRsEtO.jpeg";
+	sha256 = "sha256-def6O2CEjubDfKiL/d7h9IE+Q5bXa4iY093nzl0t8oA=";
 
 	# Sometimes stylix does not generate enough contrast for text.
 	# This setting forces white text to ensure contrast on dark backgrounds.
