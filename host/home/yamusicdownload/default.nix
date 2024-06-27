e980f9fd1666963b48b540bd6527ef8ed5b73dd1
Dmitry Voronin <hi@voronind.com>
2024-06-27 04:39:42 +0300

    YaMisicDownload : Add temporary fix.

--- host/home/yamusicdownload/default.nix
+++ host/home/yamusicdownload/default.nix
@@ -12,9 +12,17 @@
 	];
 
 	src = pkgs.fetchFromGitHub {
-		owner = "llistochek";
+		owner = "voronind-com";
 		repo  = "yandex-music-downloader";
-		rev   = "184d1d1aeb056d6b90df03437b2edde88661b19d";
-		hash  = "sha256-7ll1sMTE/fosgTRtaF6vHBsnDNiqxjFXaRG21JV7wo4=";
+		rev   = "1d105ad022be480c1746e9351ec5837ac5f4d54d";
+		hash  = "sha256-4X6tizUotL0e68vZg7hOQJf9SDQy2Drg8Hukwfvt/Ts=";
 	};
+
+	# NOTE: Waiting for PR https://github.com/llistochek/yandex-music-downloader/pull/49
+	# src = pkgs.fetchFromGitHub {
+	# 	owner = "llistochek";
+	# 	repo  = "yandex-music-downloader";
+	# 	rev   = "184d1d1aeb056d6b90df03437b2edde88661b19d";
+	# 	hash  = "sha256-7ll1sMTE/fosgTRtaF6vHBsnDNiqxjFXaRG21JV7wo4=";
+	# };
 }
