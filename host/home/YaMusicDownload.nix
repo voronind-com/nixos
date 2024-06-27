4b281d4a4dcf683692e332d0fb443ecdf4b07694
Dmitry Voronin <hi@voronind.com>
2024-06-28 00:10:37 +0300

    YaMusicDownload : Switch to my new PR.

--- host/home/YaMusicDownload.nix
+++ host/home/YaMusicDownload.nix
@@ -14,7 +14,7 @@ in {
 
 				cd ${storage}
 
-				${package}/bin/yandex-music-downloader --browser "firefox" --hq --add-lyrics --embed-cover --skip-existing --stick-to-artist --only-music --path-pattern "${pattern}" --url https://music.yandex.ru/users/cakee.ru/playlists/3 && notify_silent "Music download complete." || notify_silent "Failed to download music!"
+				${package}/bin/yandex-music-downloader --browser "firefox" --hq --skip-broken --add-lyrics --embed-cover --skip-existing --stick-to-artist --only-music --path-pattern "${pattern}" --url https://music.yandex.ru/users/cakee.ru/playlists/3 && notify_silent "Music download complete." || notify_silent "Failed to download music!"
 			'');
 		in util.mkStaticSystemdService {
 			enable = true;
--- host/home/yamusicdownload/default.nix
+++ host/home/yamusicdownload/default.nix
@@ -14,15 +14,15 @@
 	src = pkgs.fetchFromGitHub {
 		owner = "voronind-com";
 		repo  = "yandex-music-downloader";
-		rev   = "1d105ad022be480c1746e9351ec5837ac5f4d54d";
-		hash  = "sha256-4X6tizUotL0e68vZg7hOQJf9SDQy2Drg8Hukwfvt/Ts=";
+		rev   = "8266917793d17fd9dd020154bb5aabc44f88b129";
+		hash  = "sha256-lhEdeN9Vl4wcptWbF/fNfD3KqAdShs6MLYfJK5IDdXE=";
 	};
 
 	# NOTE: Waiting for PR https://github.com/llistochek/yandex-music-downloader/pull/49
 	# src = pkgs.fetchFromGitHub {
 	# 	owner = "llistochek";
 	# 	repo  = "yandex-music-downloader";
-	# 	rev   = "184d1d1aeb056d6b90df03437b2edde88661b19d";
-	# 	hash  = "sha256-7ll1sMTE/fosgTRtaF6vHBsnDNiqxjFXaRG21JV7wo4=";
+	# 	rev   = "";
+	# 	hash  = "";
 	# };
 }
