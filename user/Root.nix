f068155c190d9b0f3c0b3b02180511a8c703f4b8
Dmitry Voronin <hi@voronind.com>
2024-06-27 16:34:58 +0300

    Root : Fix common setup.

--- user/Root.nix
+++ user/Root.nix
@@ -1,16 +1,7 @@
-{ const
-, util
-, style
-, pkgs
-, config
-, key
-, secret
-, ... } @args: {
-	imports = [
-		./common
-	];
+{ secret, ... }: {
+	imports = [ ./common ];
 
-	# user.common.users = [{ name = "root"; homeDir = "/root"; }];
+	user.common.users = [{ name = "root"; homeDir = "/root"; }];
 	users.users.root.hashedPassword = secret.hashedPassword;
 	security.sudo = {
 		enable = false;
