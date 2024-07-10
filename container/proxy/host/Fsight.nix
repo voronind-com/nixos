4b60ef5e10937ced966ef8bef35edc0d409ea4b8
Dmitry Voronin <hi@voronind.com>
2024-07-10 16:09:45 +0300

    Fsight : Add master proxy.

--- container/proxy/host/Fsight.nix
+++ container/proxy/host/Fsight.nix
@@ -64,4 +64,21 @@ in {
 			ssl_dhparam /etc/letsencrypt/conf/ssl-dhparams.pem;
 		'';
 	};
+
+	"fmp-master.${domain}" = container.mkServer {
+		extraConfig = util.trimTabs ''
+			listen 443 ssl;
+			set $fmpmaster ${config.container.host}:8081;
+
+			location / {
+				proxy_pass http://$fmpmaster$request_uri;
+				add_header Referrer-Policy 'origin';
+			}
+
+			ssl_certificate /etc/letsencrypt/live/${config.container.domain}/fullchain.pem;
+			ssl_certificate_key /etc/letsencrypt/live/${config.container.domain}/privkey.pem;
+			include /etc/letsencrypt/conf/options-ssl-nginx.conf;
+			ssl_dhparam /etc/letsencrypt/conf/ssl-dhparams.pem;
+		'';
+	};
 }
