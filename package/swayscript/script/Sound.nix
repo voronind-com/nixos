950d4be9e5f7ee8bc970af75452fbb53ac3c04ba
Dmitry Voronin <hi@voronind.com>
2024-07-28 23:37:49 +0300

    Swayscript : Fix sound output array access.

--- package/swayscript/script/Sound.nix
+++ package/swayscript/script/Sound.nix
@@ -4,7 +4,7 @@
 			local IFS=$'\n'
 			local current=$(pactl get-default-sink)
 			local all=($(pactl list short sinks | cut -f2))
-			local i_current=$(_index_of ''${current} ''${all})
+			local i_current=$(_index_of ''${current} ''${all[@]})
 			local i_total=''${#all[@]}; ((i_total--))
 			local i_target=0
 
