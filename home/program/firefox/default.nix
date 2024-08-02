db77ece3c3b7e06bf9a9d96dae7f2b24b75a8bc3
Dmitry Voronin <hi@voronind.com>
2024-08-02 19:33:16 +0300

    Firefox : Disable http3.

--- home/program/firefox/default.nix
+++ home/program/firefox/default.nix
@@ -38,6 +38,7 @@
 
 		// User agent.
 		// user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36");
+		user_pref("network.http.http3.enable", false)
 	'';
 
 	mkExtension = install_url: {
